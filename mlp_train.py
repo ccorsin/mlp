9aabb5afc09c0fbcaab919b0abfdf29aeeb96633
Celia CORSIN <ccorsin@student.42.fr>
2019-06-06 17:12:29 +0200

    Add perfo metrics

--- mlp_train.py
+++ mlp_train.py
@@ -17,12 +17,12 @@ class Train:
         self.visu = visu
         self.epochs = epochs
 
-    def train(self, net):
+    def train(self, net, details):
         # sns.set(style="ticks", color_codes=True)
         # sns.pairplot(self.data, hue = 'M')
         # plt.tight_layout()
         # plt.savefig('pair_plot_selected.pdf')
-        network = Network([23, 6, 4, 2], net).gardient_descent(self.data, self.epochs, self.lr, self.visu)
+        network = Network([23, 6, 4, 2], net).gardient_descent(self.data, self.epochs, self.lr, self.visu, details)
         np.save('network.npy', network)
 
 class Network:
@@ -112,6 +112,12 @@ class Network:
         acc = (len(y) - error) / len(y)
         return acc
 
+    def ft_precision_evaluation(self, prediction, y):
+        tp = np.count_nonzero(np.argmax(prediction, axis=1) + np.argmax(y, axis=1) == 0)
+        fp_tp = np.count_nonzero(np.argmax(prediction, axis=1) == 0)
+        precision = tp / (fp_tp)
+        return precision
+
     def backward_propagation(self, output, y, caches):
         gradients = []
         m = y.shape[0]
@@ -138,7 +144,7 @@ class Network:
             self.network[i]['W'] = self.network[i]['W'] - lr * gradient[i]['dW']
             self.network[i]['b'] = self.network[i]['b'] - lr * gradient[i]['db']
 
-    def gardient_descent(self, data, epochs, lr, visu):
+    def gardient_descent(self, data, epochs, lr, visu, details):
         y = self.build_excpected(data)
         data = data.iloc[:, 1:]
         minmax = self.ft_get_stats(data)
@@ -150,15 +156,20 @@ class Network:
         plot_tr_loss = []
         plot_val_loss = []
         plot_tr_acc = []
+        val_cost = 0
         for epoch in range(epochs):
-            outputs, caches = self.forward_propagation(tr_set)
-            cost = self.ft_cost_evaluation(outputs, tr_y)
-            gradients = self.backward_propagation(outputs, tr_y, caches)
+            tr_outputs, caches = self.forward_propagation(tr_set)
+            cost = self.ft_cost_evaluation(tr_outputs, tr_y)
+            gradients = self.backward_propagation(tr_outputs, tr_y, caches)
             self.update_weights(tr_set, lr, gradients)
-            tr_acc = self.ft_acc_evaluation(outputs, tr_y)
+            tr_acc = self.ft_acc_evaluation(tr_outputs, tr_y)
             val_outputs, _ = self.forward_propagation(val_set)
+            val_cost_previous = val_cost
             val_cost = self.ft_cost_evaluation(val_outputs, val_y)
-            if val_cost < cost and epoch > 1000:
+            tr_prec = self.ft_precision_evaluation(tr_outputs, tr_y)
+            val_acc = self.ft_acc_evaluation(val_outputs, val_y)
+            val_prec = self.ft_precision_evaluation(val_outputs, val_y)
+            if val_cost > val_cost_previous and epoch:
                 with open('minmax.json', 'w+') as json_file:  
                     json.dump(minmax, json_file)
                     if visu:
@@ -168,12 +179,14 @@ class Network:
                         plt.show()
                     np.save('network.npy', self.network)
                 exit()
-            val_acc = self.ft_acc_evaluation(val_outputs, val_y)
             if visu:
                 plot_tr_loss.append(cost)
                 plot_tr_acc.append(tr_acc)
                 plot_val_loss.append(val_cost)
-            print('>epoch=%d, lrate=%.4f, loss=%.3f, acc=%.3f, val_loss=%.3f, val_acc=%.3f' % (epoch, lr, cost, tr_acc, val_cost, val_acc))
+            if details:
+                print('>epoch=%d, lrate=%.4f, loss=%.3f, acc=%.3f, prec=%.3f, val_loss=%.3f, val_acc=%.3f, val_prec=%.3f' % (epoch, lr, cost, tr_acc, tr_prec, val_cost, val_acc, val_prec))
+            else:
+                print('>epoch=%d, lrate=%.4f, loss=%.3f, val_loss=%.3f' % (epoch, lr, cost, val_cost))
         with open('minmax.json', 'w+') as json_file:  
             json.dump(minmax, json_file)
         if visu:
@@ -186,9 +199,10 @@ class Network:
 args = argparse.ArgumentParser("Statistic description of your data file")
 args.add_argument("file", help="File to descripte", type=str)
 args.add_argument("-e", "--epoch", help="The number of iterations to go through the regression", default=5000)
-args.add_argument("-l", "--learning", help="The learning rate to use during the regression", default=0.1, type=float)
+args.add_argument("-l", "--learning", help="The learning rate to use during the regression", default=0.2, type=float)
 args.add_argument("-v", "--visu", help="Visualize functions", action="store_true", default=False)
-args.add_argument("-n", "--net", help="Specific network input", type=str, default=False)
+args.add_argument("-n", "--network", help="Specific network input", type=str, default=False)
+args.add_argument("-d", "--details", help="Display more performance metrics", action="store_true", default=False)
 args = args.parse_args()
 
 if os.path.isfile(args.file):
@@ -196,7 +210,7 @@ if os.path.isfile(args.file):
         df = pd.read_csv(args.file, sep=',')
         df = df.dropna()
         df = df.iloc[:, [1, 2, 3, 6, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 21, 22, 23, 26, 27, 28, 29, 30]]
-        network = Train(df, args.epoch, args.learning, args.visu).train(args.net)
+        network = Train(df, args.epoch, args.learning, args.visu).train(args.network, args.details)
         
     except Exception as e:
         raise(e)
